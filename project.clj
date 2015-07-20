9263709c5984adb02b120ada4054b0a0ea298b36
Max Langensiepen <mlangens@emich.edu>
2015-07-20 18:21:12 -0400

    lein run and file getting from url

--- project.clj
+++ project.clj
@@ -8,6 +8,7 @@
                  [ring/ring-jetty-adapter "1.3.2"]
                  [compojure "1.4.0"]]
   :plugins [[lein-ring "0.8.13"]]
+  :main ncopter-backend.core
   :ring {:handler ncopter-backend.core/app}
   :profiles {:dev {:plugins [[lein-clojars "0.9.1"]
                                [lein-midje "3.1.3"]]
--- src/ncopter_backend/core.clj
+++ src/ncopter_backend/core.clj
@@ -1,8 +1,18 @@
+(use 'ring.middleware.resource)
+(use 'ring.adapter.jetty)
+(use 'ring.middleware.resource)
+
 (ns ncopter-backend.core
   (:require [compojure.core :refer :all]
-            [compojure.route :as route]))
+            [compojure.route :as route]
+            [ring.util.response :as resp]))
 
 (defroutes app
+;    (resp/resource-response "max" {:root "public"}))
   (GET "/cargos/:cargo" [cargo]
-    (str "Cargo is " cargo))
-  (route/not-found "not here"))
+    (resp/file-response (str cargo ".jpg")
+        {:root "/Users/mlangens/Documents/workspace/ncopter-backend/resources/public"}))
+  (route/not-found "not  here"))
+
+(defn -main []
+   (ring.adapter.jetty/run-jetty app {:port 3000}))
